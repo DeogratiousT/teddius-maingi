f0e9c5153806b25e146bbeeef898bcd96fe8018e
Teddius Maingi <maingi.teddius@thetag.co.ke>
2022-01-22 11:20:06 +0300

    resume

--- resources/views/pages/resume.blade.php
+++ resources/views/pages/resume.blade.php
@@ -3,63 +3,67 @@
 
       <div class="section-title">
         <h2>Resume</h2>
-        <p>Check My Resume</p>
       </div>
 
       <div class="row">
         <div class="col-lg-6">
-          <h3 class="resume-title">Sumary</h3>
           <div class="resume-item pb-0">
-            <h4>Alice Barkley</h4>
-            <p><em>Innovative and deadline-driven Graphic Designer with 3+ years of experience designing and developing user-centered digital/print marketing material from initial concept to final, polished deliverable.</em></p>
+            <h4>Teddius Maingi</h4>
+            <p><em>Innovative and deadline-driven Software Developer with 3+ years of experience designing and developing web based and android software solutions from initial concept to final, polished deliverable.</em></p>
             <p>
             <ul>
-              <li>Portland par 127,Orlando, FL</li>
-              <li>(123) 456-7891</li>
-              <li>alice.barkley@example.com</li>
+              <li>Nairobi, Kenya</li>
+              <li>+254 795 615409</li>
+              <li>teddiusmaingi@gmail.com</li>
             </ul>
             </p>
           </div>
 
           <h3 class="resume-title">Education</h3>
-          <div class="resume-item">
+          {{-- <div class="resume-item">
             <h4>Master of Fine Arts &amp; Graphic Design</h4>
             <h5>2015 - 2016</h5>
             <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
             <p>Qui deserunt veniam. Et sed aliquam labore tempore sed quisquam iusto autem sit. Ea vero voluptatum qui ut dignissimos deleniti nerada porti sand markend</p>
-          </div>
+          </div> --}}
           <div class="resume-item">
-            <h4>Bachelor of Fine Arts &amp; Graphic Design</h4>
-            <h5>2010 - 2014</h5>
-            <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
-            <p>Quia nobis sequi est occaecati aut. Repudiandae et iusto quae reiciendis et quis Eius vel ratione eius unde vitae rerum voluptates asperiores voluptatem Earum molestiae consequatur neque etlon sader mart dila</p>
+            <h4>Bachelor of Science in Information Technology</h4>
+            <h5>2016 - 2019</h5>
+            <p><em>Dedan Kimathi University of Technology, Nyeri - Kenya</em></p>
+            {{-- <p>Quia nobis sequi est occaecati aut. Repudiandae et iusto quae reiciendis et quis Eius vel ratione eius unde vitae rerum voluptates asperiores voluptatem Earum molestiae consequatur neque etlon sader mart dila</p> --}}
           </div>
         </div>
         <div class="col-lg-6">
           <h3 class="resume-title">Professional Experience</h3>
           <div class="resume-item">
-            <h4>Senior graphic design specialist</h4>
-            <h5>2019 - Present</h5>
-            <p><em>Experion, New York, NY </em></p>
+            <h4>SOFTWARE DEVELOPER</h4>
+            <h5>Jan 2021 - Present</h5>
+            <p><em>Kenya National Innovation Agency</em></p>
             <p>
             <ul>
-              <li>Lead in the design, development, and implementation of the graphic, layout, and production communication materials</li>
-              <li>Delegate tasks to the 7 members of the design team and provide counsel on all aspects of the project. </li>
-              <li>Supervise the assessment of all graphic materials in order to ensure quality and accuracy of the design</li>
-              <li>Oversee the efficient use of production project budgets ranging from $2,000 - $25,000</li>
+              <li>Lead in the analysis and design, development, and implementation of the Software Systems</li>
+              <li>Summit Administrator for the Technology and 4 <sup>th</sup> Industrial Revolution Summit of the Kenya Innovation Week 2021</li>
+              <li>ICT Support tasks including hardware installations and troubleshoooting and LAN installations, configurations and troubleshooting</li>
             </ul>
             </p>
           </div>
           <div class="resume-item">
-            <h4>Graphic design specialist</h4>
-            <h5>2017 - 2018</h5>
-            <p><em>Stepping Stone Advertising, New York, NY</em></p>
+            <h4>Part-time Softare Developer</h4>
+            <h5>Dec 2020 - March 2021</h5>
+            <p><em>Techxers Ltd</em></p>
             <p>
             <ul>
-              <li>Developed numerous marketing programs (logos, brochures,infographics, presentations, and advertisements).</li>
-              <li>Managed up to 5 projects or tasks at a given time while under pressure</li>
-              <li>Recommended and consulted with clients on the most appropriate graphic design</li>
-              <li>Created 4+ design presentations and proposals a month for clients and account managers</li>
+              <li>Developed a tobacco management System</li>
+            </ul>
+            </p>
+          </div>
+          <div class="resume-item">
+            <h4>Intern</h4>
+            <h5>Feb 2020 - Aug 2020</h5>
+            <p><em>Centrisign</em></p>
+            <p>
+            <ul>
+              <li>Part of a team that developed the UZA360 platform</li>
             </ul>
             </p>
           </div>
